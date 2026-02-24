8bbaea94561810bd1b2400c03e426eb088f8117d
Ajayraj1113 <ajayraj824@gmail.com>
2026-02-24 12:50:07 +0530

    update functionality done

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createTodo, getTodo, getSingleTodo } = require("../controllers/todoControllers");
+const { createTodo, getTodo, getSingleTodo, updateTodo } = require("../controllers/todoControllers");
 
 router.post("/", createTodo);
 router.get("/", getTodo);
 router.get("/:id", getSingleTodo);
-// router.put("/:id", handler);
+router.put("/:id", updateTodo);
 // router.delete("/:id", handler );
 
 module.exports = router;
